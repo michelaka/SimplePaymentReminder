e7fe9883e50d2452cbdf10894f9af979d3967b62
Michel Aka <michel.aka@Gmail.com>
2017-10-02 14:29:36 -0700

    First Commit

---
+++ LateListRem2/LateListRem2/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using LateListRem2.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+
+namespace LateListRem2
+{
+    public partial class MainPage : ContentPage
+    {
+        
+        public MainPage()
+        {
+            InitializeComponent();
+
+            mLisView.ItemsSource = GetItemList("");
+        }
+
+        private void mLisView_Refreshing(object sender, EventArgs e)
+        {
+            mLisView.ItemsSource = GetItemList("");
+            mLisView.EndRefresh();
+        }
+
+
+
+        List<LateItem> GetItemList(string searchString)
+        {
+            var itemsList = new List<LateItem> {
+                new LateItem(){CustomerName ="Cust1", TransactionDate = System.DateTime.Today },
+                new LateItem(){CustomerName ="Cust2", TransactionDate = System.DateTime.Today.AddDays(-1) }
+
+            };
+
+            if (String.IsNullOrWhiteSpace(searchString))
+                return itemsList;
+
+            return itemsList.Where(c => c.CustomerName.ToLower().Contains(searchString.ToLower())).ToList();
+
+        }
+
+        private void SearchBar_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            mLisView.ItemsSource = GetItemList(e.NewTextValue);
+        }
+    }
+}
---
+++ LateListRem2/LateListRem2/Model/LateItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LateListRem2.Model
+{
+    class LateItem
+    {
+        public string CustomerName { get; set; }
+        public string ImageURL { get; set; }
+        public DateTime TransactionDate { get; set; }
+    }
+}
